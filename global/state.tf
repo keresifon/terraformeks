430c53caa29c96537aee2d1421c19abafff76b72
Keresifon <keresifon@gmail.com>
2021-11-02 22:59:49 +0100

    Update state.tf

--- global/state.tf
+++ global/state.tf
@@ -2,11 +2,11 @@
 
 terraform {
   backend "s3" {
-    bucket = var.s3_bucket
+    bucket = "enobasin"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-1"
 
-    dynamodb_table = var.db_name
+    dynamodb_table = "terraform-lock"
     encrypt = true
   }
 }
